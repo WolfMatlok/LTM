41f160017cf71f327ac12b8c5b51fa52a3417f0f
Harry Steiner <harrysteiner@gmx.net>
2015-09-27 17:59:22 +0200

    cPlayer enhanced.

--- cPlayer.cpp
+++ cPlayer.cpp
@@ -7,16 +7,16 @@
 
 #include "cPlayer.h"
 
-cPlayer::cPlayer(int p_iId)
+int cPlayer::s_iGamesToPlayMax = 0;
+
+cPlayer::cPlayer(int p_iId, int p_iGamesToPlay)
 : m_iId(p_iId)
-, m_iGamesToPlayMax(p_iGamesToPlayMax)
-, m_iGamesToPlay(0)
+, m_iGamesToPlay(p_iGamesToPlay)
 {
 }
 
 cPlayer::cPlayer(const cPlayer& orig)
 {
-  m_iGamesToPlayMax = orig.m_iGamesToPlayMax;
   m_iId = orig.m_iId;
   m_iGamesToPlay = orig.m_iGamesToPlay;
 }
--- cPlayer.h
+++ cPlayer.h
@@ -8,23 +8,31 @@
 #ifndef CPLAYER_H
 #define	CPLAYER_H
 
+#include <iostream>
+
 class cPlayer
 {
 public:
-  cPlayer(int p_iId);
+  cPlayer(int p_iId, int p_iGamesToPlay = 0);
   cPlayer(const cPlayer& orig);
   virtual ~cPlayer();
   
-  bool CantPlayMoreGames(){return m_iGamesToPlay >= m_iGamesToPlayMax;}
-  
+  bool CanPlayMoreGames(){return m_iGamesToPlay < s_iGamesToPlayMax;}  
   int GameRegister(){m_iGamesToPlay+=1; return m_iGamesToPlay;}
   int GameUnregister(){m_iGamesToPlay-=1; if(m_iGamesToPlay<0){m_iGamesToPlay=0;} return m_iGamesToPlay;}
-
+  static void SetGamesToPlay(int p_iGamesToPlay){cPlayer::s_iGamesToPlayMax = p_iGamesToPlay;}
+  
+  friend std::ostream& operator<<(std::ostream& os, const cPlayer& p_oPlayer)
+  {
+    //os << "ID:" << std::setw(5) << p_oPlayer.m_iId << " GamesToPlay:" << std::setw(2) << p_oPlayer.m_iGamesToPlay; 
+    os << std::setw(5) << p_oPlayer.m_iId;
+    return os;
+  }
+  
 private:
   int m_iId;
   int m_iGamesToPlay;
-  static int m_iGamesToPlayMax;
-
+  static int s_iGamesToPlayMax;
 };
 
 #endif	/* CPLAYER_H */
